b732674b468290d71184935fd794d8f8d62d9e31
NikhilSharmaProjects <nikhil3265.1947@gmail.com>
2026-03-23 10:37:20 +0530

    Creating new Frontend

--- android/app/src/main/java/app/lovable/df95e9c254e64fa390b783e89055b20e/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package app.lovable.df95e9c254e64fa390b783e89055b20e;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- supabase/migrations/20251123132937_26254092-ec83-428c-9b3c-1afeeddd3234.sql
+++
@@ -1,4 +0,0 @@
--- Add new enum values for app_role
-ALTER TYPE public.app_role ADD VALUE IF NOT EXISTS 'super_admin';
-ALTER TYPE public.app_role ADD VALUE IF NOT EXISTS 'hod';
-ALTER TYPE public.app_role ADD VALUE IF NOT EXISTS 'teacher';
